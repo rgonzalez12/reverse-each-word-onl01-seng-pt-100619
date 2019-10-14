0cadc79e06a75d24e8cd5babd5a0deef91a73126
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-14 07:43:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
-def reverse_each_word(string)
-  arr = []
-  array.collect do |string|
-    arr.reverse << string
-  end
-  arr
+def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
 end
